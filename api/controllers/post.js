7df8a2e2a8e325ab0b0b741d590a1f7ebfdd366f
amitinho <130788510+joao7leibo@users.noreply.github.com>
2023-08-29 14:12:27 +0300

    Add files via upload

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,70 +1,75 @@
-import { db } from "../connect.js";
+import {db} from "../connect.js";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
-export const getPosts = (req, res) => {
-  const userId = req.query.userId;
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
+export const getPosts =(req,res)=>{
 
-    console.log(userId);
+    const userId = req.query.userId;
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!");
 
-    const q =
-      userId !== "undefined"
-        ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC`
-        : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
-    LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId= ? OR p.userId =?
-    ORDER BY p.createdAt DESC`;
+    jwt.verify(token, "secretKey", (err, userInfo)=>{
+        if(err) return res.status(403).json("Token is not valid!");
 
-    const values =
-      userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
-
-    db.query(q, values, (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.status(200).json(data);
+        console.log(userId ?"there is" : "not")
+    
+        const q = userId !== "undefined"
+            ? 'SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC' 
+            : 'SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC';
+      
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
+      
+        db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json(data);
+            
+        });
+        console.log(values)
     });
-  });
 };
 
-export const addPost = (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
+export const addPost =(req, res)=>{
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!");
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const q =
-      "INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
-    const values = [
-      req.body.desc,
-      req.body.img,
-      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
-      userInfo.id,
-    ];
-
-    db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.status(200).json("Post has been created.");
+    jwt.verify(token, "secretKey", (err, userInfo)=>{
+        if(err) return res.status(403).json("Token is not valid!");
+    
+        const q = 'INSERT INTO posts(`desc`, `img`, `createdAt`, `userId`) VALUES (?, ?, ?, ?)';
+        const values = [
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id,
+        ];
+    
+        db.query(q, values, (err, data)=>{
+            if(err) return res.status(500).json(err);
+            return res.status(200).json("Post  has been created");
+      });
     });
-  });
 };
-export const deletePost = (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
+export const deletePost =(req, res)=>{
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("Not logged in!");
 
-    const q =
-      "DELETE FROM posts WHERE `id`=? AND `userId` = ?";
-
-    db.query(q, [req.params.id, userInfo.id], (err, data) => {
-      if (err) return res.status(500).json(err);
-      if(data.affectedRows>0) return res.status(200).json("Post has been deleted.");
-      return res.status(403).json("You can delete only your post")
+    jwt.verify(token, "secretKey", (err, userInfo)=>{
+        if(err) return res.status(403).json("Token is not valid!");
+    
+        const q = 'DELETE FROM posts WHERE id=? AND userId = ? ';
+        const values = [
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id,
+        ];
+    
+        db.query(q, [req.params.id, userInfo.id], (err, data)=>{
+            if(err) return res.status(500).json(err);
+            if (data.affectedRows>0) return res.status(200).json("Post  has been deleted");
+            return res.status(403).json("You can delete only your post");
+      });
     });
-  });
 };
